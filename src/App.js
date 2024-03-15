fdc4c01bd8454bd38d9a271b62412319bdf8a1de
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-03-15 00:57:54 +0000

    inisializing react structure by LAABID ABDESSAMAD at 15-03-2024 00:57

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>
+          <span>hr</span> pro
+        </h1>
+        <ul>
+          <li className="dark-ground"></li>
+          <li className="translation"></li>
+          <li className="notifications"></li>
+          <li className="account"></li>
+        </ul>
+      </header>
+      <main></main>
+      <aside></aside>
+    </>
+  );
+}
+
+export default App;
