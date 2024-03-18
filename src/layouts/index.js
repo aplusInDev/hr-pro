db260e6c9afcee920934a054cb29c04f6cf7b680
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-03-18 19:54:18 +0000

    changing main tag position to the Main commponent by aplus at 19:54 18-03-2024

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Sidebar } from './Sidebar';
+export { default as Main } from './Main';
 export { default as Footer } from './Footer';
