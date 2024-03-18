78131eeffe2b6d384a0a2f409ffa63500e4f05c8
farah-am <155267849+farah-am@users.noreply.github.com>
2024-03-18 05:36:34 +0100

    farah sidebar first attempt 04:36 18/03

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
   return (
     <header>
         <h1>
-            <span>hr</span> pro
+          <span>hr</span> pro
         </h1>
         <ul>
-        <li className="dark-ground"></li>
-        <li className="translation"></li>
-        <li className="notifications"></li>
-        <li className="account"></li>
+          <li className="dark-ground"></li>
+          <li className="translation"></li>
+          <li className="notifications"></li>
+          <li className="account"></li>
         </ul>
     </header>
   );
