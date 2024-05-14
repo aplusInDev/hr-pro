c5194e0486deebcfed99804b99d07b45ef903949
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-05-14 20:23:36 +0100

    removing unimportant lines from Employees component

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -3,31 +3,18 @@ import { Info, Filter } from '../components';
 import { Btn } from '../components/ui';
 import '../assets/css/Employees.css';
 import { Outlet, Link, useLoaderData } from 'react-router-dom';
-// import httpClient from '../services/httpClient';
 
 export default function Employees() {
   const {employees, employeeFields} = useLoaderData();
-  // const [employeeInfo, setEmployeeInfo] = useState(null);
   const [active, setActive] = useState(null);
 
   function handleClick(e) {
     if (active === e.target.id) {
       setActive(null);
-      // setEmployeeInfo(null);
     } else {
       setActive(e.target.id);
-      // getEmployee(e.target.id);
     }
   }
-
-  // async function getEmployee(employee_id) {
-  //   try {
-  //     const response = await httpClient.get(`/employees/${employee_id}`);
-  //     setEmployeeInfo(response.data);
-  //   } catch (err) {
-  //     console.log('erro: ', err);
-  //   }
-  // }
 
   return (
     <>
