d3a15d371ac8d2b7d7cc27e4c935371d606e66f0
farah-am <155267849+farah-am@users.noreply.github.com>
2024-05-15 19:21:09 +0100

    this is the landing page done by Farah

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,22 +6,28 @@ import "../assets/css/LandingPage.css";
 export default function LandingPage() {
   return (
     <div className="landing-page">
-      <header>
+      <body >
         <div className="logo">
           <span>hr</span><span>pro</span>
         </div>
-        <div className="btns">
-          <button type='button'><Link to={'login'}>Login In</Link></button>
-          <button type='button'><Link to={'register'}>Sign Up</Link></button>
-        </div>
-      </header>
+       
       <Alert
         title="Welcome"
         body="Welcome to the landing page"
-      />
-      <section>
-        <h1>Welcome to the Landing Page</h1>
-      </section>
+      /> 
+   
+      
+        <h1 className='t1'>Empower your team with <span className='title'>HRpro</span></h1>
+        <p>As your company grows, so do the opportunities available to it.<br></br>
+           By partnering with us, you will benefit from many advantages 
+           and<br></br> you will give your  team every means to succeed.
+        </p>
+        <div className="btns">
+          <button type='button'><Link to={'login'}>Login In</Link></button>                                                                                                                                                               
+          <button type='button'><Link to={'register'}>Sign Up</Link></button>
+        </div>
+    
+      </body>
     </div>
   );
 }
