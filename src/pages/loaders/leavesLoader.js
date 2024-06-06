e8714b384cf02cf8ec2e5afa351ce2810dd8a480
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-06-06 19:36:29 +0100

    removing unimportant line from leavesLoader

--- src/pages/loaders/leavesLoader.js
+++ src/pages/loaders/leavesLoader.js
@@ -14,7 +14,6 @@ export default async function leavesLoader() {
       url = `/leaves?company_id=${company_id}&year=2024`;
     }
     const response = await httpClient.get(url);
-    console.log(response.data);
 
     return response.data;
   } catch (err) {
