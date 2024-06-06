b4af8f50b9bc395510da2a8b357b52085296a701
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-06-06 16:53:29 +0100

    updating hr absences page

--- src/pages/Absences.jsx
+++ src/pages/Absences.jsx
@@ -5,6 +5,7 @@ import { useLoaderData } from 'react-router-dom';
 import { excelFileReader } from '../utils/excelUtils';
 import httpClient from '../services/httpClient';
 import { Icon } from '@iconify/react';
+import '../assets/css/Absences.css';
 
 export default function Employees() {
   const role = JSON.parse(localStorage.getItem('currentUser'))?.role;
@@ -52,7 +53,7 @@ export default function Employees() {
     <>
       {/* <Filter />
       <Outlet /> */}
-      <section className="employees-container">
+      <section className="employees-container absences-container">
         {error && <h2 className='error'>{error}</h2>}
         {role === 'employee'? (
             <AbsencesTable data={absences} />
@@ -64,20 +65,36 @@ export default function Employees() {
               className={show && employee.id === activeId ? 'show' : 'hide'}
             >
               <div className='absent-info'>
-                <span
-                  id={employee.id}
-                  onClick={(e) => {
-                    if(employee.absences === 0) return;
-                    handleClick(e);
-                  }}
-                  >
-                  {employee.first_name} {employee.last_name}
-                </span>
-                <span className='sum_absences'>
-                  {employee.absences} absences
-                </span>
-                <div>
-                  {employee.position_info.job_title}
+                <div className='employee-short-info'>
+                  <span
+                    id={employee.id}
+                    onClick={(e) => {
+                      if(employee.absences === 0) return;
+                      handleClick(e);
+                    }}
+                    >
+                    {employee.first_name} {employee.last_name}
+                  </span>
+                  <span>
+                    {employee.position_info.job_title}
+                  </span>
+                </div>
+                <div className="absences-short-info">
+                  {(employee.id !== activeId || !show) && (
+                    <span className='sum_absences'>
+                      {employee.absences_total_days} day in {employee.absences} absences
+                    </span>
+                  )}
+                  {employee.absences > 0 && (employee.id !== activeId || !show) && (
+                    <>
+                      <span className='justified-absences'>
+                        {employee.justified_absences_days} day in {employee.justified_absences} justified absences
+                      </span>
+                      <span className='unjustified-absences'>
+                        {employee.unjustified_absences_days} day in {employee.unjustified_absences} justified absences
+                      </span>
+                    </>
+                  )}
                 </div>
               </div>
               {employee.id === activeId && data ? (
