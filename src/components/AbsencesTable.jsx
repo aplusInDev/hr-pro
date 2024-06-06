7fbc20c2da65db96f0d97566e4980a4302b71f87
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-06-06 19:26:34 +0100

    editing absence reason

--- src/components/AbsencesTable.jsx
+++ src/components/AbsencesTable.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Btn } from './ui';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 export default function AbsencesTable(data) {
+  const [editStatus, setEditStatus] = useState("idle"); // idle, editing, submitting
+  const [changedData, setChangedData] = useState("");
+  const role = JSON.parse(localStorage.getItem('currentUser'))?.role;
+
+  function handleChange(e) {
+    setEditStatus("editing");
+    setChangedData(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setEditStatus("submitting");
+    setTimeout(() => {
+      setEditStatus("idle");
+      console.log(changedData);
+    }, 2*1000);
+  }
+
   if (data) {
     data = data.data;
     if (data.length < 1) {
@@ -27,8 +47,26 @@ export default function AbsencesTable(data) {
                   rows='2'
                   cols='30'
                   defaultValue={row.reason}
-                  readOnly
+                  onChange={handleChange}
+                  readOnly={editStatus !== "editing"}
                 />
+                {editStatus === "submitting" && (
+                  <p>Updating..</p>
+                )}
+                {editStatus === "idle" && role !== "employee" && (
+                  <Btn text=""
+                    onClick={() => setEditStatus("editing")}
+                  >
+                    <Icon icon="akar-icons:edit" />
+                  </Btn>
+                )}
+                {editStatus === "editing" && (
+                  <Btn text=""
+                    onClick={handleSubmit}
+                  >
+                    <Icon icon="akar-icons:check" />
+                  </Btn>
+                )}
               </td>
             </tr>
           ))}
