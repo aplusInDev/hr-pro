10e6569d027a301a8ddf7bbf8579c1ffbb43896d
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-07-30 21:30:31 +0100

    updating form and filed services

--- src/services/fieldService.js
+++ src/services/fieldService.js
@@ -1,68 +1,115 @@
-export async function postField(id, info) {
-  const url = `http://localhost:5000/api/v1/forms/${id}/fields`;
+import httpClient from "./httpClient";
+
+// export async function postField(id, info) {
+//   const url = `http://localhost:5000/api/v1/forms/${id}/fields`;
+//   try {
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(info)
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
+
+export async function postField(id ,info) {
   try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(info)
-    });
-    const data = await response.json();
-    return data;
+    const response = await httpClient.post(`/forms/${id}/fields`, info);
+    return response.data;
   } catch (error) {
     return null;
   }
 }
+
+// export async function getFields(formId) {
+//   const url = `http://localhost:5000/api/v1/forms/${formId}/fields`;
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
 
 export async function getFields(formId) {
-  const url = `http://localhost:5000/api/v1/forms/${formId}/fields`;
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const response = await httpClient.get(`/forms/${formId}/fields`);
+    return response.data;
   } catch (error) {
     return null;
   }
 }
 
+// export async function getAllFields(formName, companyId) {
+//   if (!formName || !companyId) {
+//     return null;
+//   }
+//   const url = `http://localhost:5000/api/v1/fields?form_name=${formName}&company_id=${companyId}`;
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
+
 export async function getAllFields(formName, companyId) {
-  if (!formName || !companyId) {
-    return null;
-  }
-  const url = `http://localhost:5000/api/v1/fields?form_name=${formName}&company_id=${companyId}`;
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const response = await httpClient.get(`/fields?form_name=${formName}&company_id=${companyId}`);
+    return response.data;
   } catch (error) {
     return null;
   }
 }
+
+// export async function putField(fieldUrl, info) {
+//   try {
+//     const response = await fetch(fieldUrl, {
+//       method: 'PUT',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(info)
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
 
 export async function putField(fieldUrl, info) {
   try {
-    const response = await fetch(fieldUrl, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(info)
-    });
-    const data = await response.json();
-    return data;
+    const response = await httpClient.put(fieldUrl, info);
+    return response.data;
   } catch (error) {
     return null;
   }
 }
 
+// export async function deleteField(fieldUrl) {
+//   try {
+//     const response = await fetch(fieldUrl, {
+//       method: 'DELETE'
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
+
 export async function deleteField(fieldUrl) {
   try {
-    const response = await fetch(fieldUrl, {
-      method: 'DELETE'
-    });
-    const data = await response.json();
-    return data;
+    const response = await httpClient.delete(fieldUrl);
+    return response.data;
   } catch (error) {
     return null;
   }
--- src/services/formService.js
+++ src/services/formService.js
@@ -1,57 +1,95 @@
+import httpClient from "./httpClient";
+
+// export async function getForms(url) {
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     return data;
+//   } catch (error) {
+
+//     return null;
+//   }
+// }
+
 export async function getForms(url) {
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return data;
+    const response = await httpClient.get(url);
+    return response.data;
   } catch (error) {
-
     return null;
   }
 }
+
+// export async function postForm(url, info) {
+//   try {
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(info)
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
 
 export async function postForm(url, info) {
   try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(info)
-    });
-    const data = await response.json();
-    return data;
+    const response = await httpClient.post(url, info);
+    return response.data;
   } catch (error) {
     return null;
   }
 }
 
+// export async function putForm(info) {
+//   try {
+//     const response = await fetch(info.uri, {
+//       method: 'PUT',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(info)
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
+
 export async function putForm(info) {
   try {
-    const response = await fetch(info.uri, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(info)
-    });
-    const data = await response.json();
-    return data;
+    const response = await httpClient.put(info.uri, info);
+    return response.data;
   } catch (error) {
     return null;
   }
 }
+
+// export async function deleteForm(id) {
+//   try {
+//     const url = `http://localhost:5000/api/v1/forms/${id}`
+//     const response = await fetch(url, {
+//       method: 'DELETE'
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return null;
+//   }
+// }
 
 export async function deleteForm(id) {
   try {
     const url = `http://localhost:5000/api/v1/forms/${id}`
-    const response = await fetch(url, {
-      method: 'DELETE'
-    });
-    const data = await response.json();
-    return data;
+    const response = await httpClient.delete(url);
+    return response.data;
   } catch (error) {
     return null;
   }
 }
-
