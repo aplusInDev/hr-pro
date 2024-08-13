51e36f37da1d4bbb25b59ff8ecc9d077195b5ee7
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-08-13 01:27:16 +0100

    updating api baseUrl

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  // baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "http://hrpro.aplusdev.tech/api/v1/",
   withCredentials: true,
 });
 
