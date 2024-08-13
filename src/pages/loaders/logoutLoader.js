06c10bffa8bc9c193831e5b4210abc289386aa44
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-08-13 02:09:09 +0100

    unimportant updates in logout loader

--- src/pages/loaders/logoutLoader.js
+++ src/pages/loaders/logoutLoader.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 
 export default async function logoutLoader() {
   try {
-    await httpClient.delete('http://localhost:5000/api/v1/logout');
+    await httpClient.delete('/logout');
     Cookies.remove('session_id');
     localStorage.removeItem('currentUser');
     return redirect('/');
